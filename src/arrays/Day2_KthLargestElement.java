b3cb06534ac9d8dd631de436d0372959b2d3cd73
zishan <zishan8291@gmail.com>
2023-01-14 23:47:53 +0530

    Modified Day 2

--- src/arrays/Day2_KthLargestElement.java
+++ src/arrays/Day2_KthLargestElement.java
@@ -1,17 +1,26 @@
 package arrays;
 
+import java.util.Arrays;
 import java.util.PriorityQueue;
 
 //215. Kth Largest Element in an Arra
 public class Day2_KthLargestElement {
-    public int findKthLargest(int[] nums, int k) {
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 1, 5, 6, 4};
+        int k = 2;
+        System.out.println( findKthLargest(nums,k));
+        System.out.println(findKthLargest1(nums,k));
+
+    }
+
+    public static int findKthLargest(int[] nums, int k) {
         int n = nums.length;
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        for(int i =0;i<k;i++){
+        for (int i = 0; i < k; i++) {
             minHeap.add(nums[i]);
         }
-        for(int i =k;i<n;i++){
-            if(nums[i]>minHeap.peek()){
+        for (int i = k; i < n; i++) {
+            if (nums[i] > minHeap.peek()) {
                 minHeap.poll();
                 minHeap.add(nums[i]);
             }
@@ -19,4 +28,10 @@ public class Day2_KthLargestElement {
         return minHeap.peek();
     }
 
+    public static int findKthLargest1(int[] nums, int k) {
+        Arrays.sort(nums);
+        return nums[nums.length-k];
+    }
+
 }
+

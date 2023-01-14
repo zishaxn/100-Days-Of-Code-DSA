42fa9ce4f84e2f8c4dae356771145ef986027578
zishan <zishan8291@gmail.com>
2023-01-14 19:11:58 +0530

    Modified Day 1 problem

--- src/arrays/Day1_TimeNeededToBuyTickets.java
+++ src/arrays/Day1_TimeNeededToBuyTickets.java
@@ -3,26 +3,11 @@ package arrays;
 public class Day1_TimeNeededToBuyTickets {
     //2073. Time Needed to Buy Tickets
     public static void main(String[] args) {
-        int[] arr = {5, 1, 1, 1};
-        System.out.println(timeToBuy(arr, 2));
+        int[] arr = {2, 3,5, 2, 4, 6, 1};         //4 ;3 2 1 0 =4 second                  //6 :5 4 3
+        System.out.println(timeToBuy(arr, 4));
 
     }
 
-    public static int timeRequiredToBuy(int[] tickets, int k) {
-        int time = 0;
-        for (int i = 0; i < tickets.length; i++) {
-            if (tickets[i] <= tickets[k]) {
-                time += tickets[i];
-            } else {
-                time += tickets[k];
-            }
-            if (i > k && tickets[i] >= tickets[k]) {
-                time--;
-            }
-        }
-        return time;
-
-    }
 
     public static int timeToBuy(int[] tickets, int k) {
         int pers = tickets[k];
